d82d7fd7ec3f1e97cbf9b06e83f39459bd86fde6
Jorundur <jorundurj@gmail.com>
2017-09-18 21:38:45 +0100

    Playing around with Physijs

--- main.js
+++ main.js
@@ -1,187 +1,172 @@
-/*
-   Three.js "tutorials by example"
-   Author: Lee Stemkoski
-   Date: July 2013 (three.js v59dev)
-*/
-
-// MAIN
-
-// standard global variables
-var container, scene, camera, renderer, controls, stats;
-var keyboard = new THREEx.KeyboardState();
-var clock = new THREE.Clock();
-
-// custom global variables
-var MovingFloor;
-var Axis;
-
-init();
-animate();
-
-// FUNCTIONS      
-function init() 
-{
-   // SCENE
-   scene = new THREE.Scene();
+'use strict';
    
-   // CAMERA
-   var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
-   var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 20000;
-   camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
-   scene.add(camera);
-   camera.position.set(0,400,1200);
-   camera.lookAt(scene.position);   
+Physijs.scripts.worker = 'physijs_worker.js';
+Physijs.scripts.ammo = 'js/ammo.js';
+
+var initScene, render, _boxes = [], spawnBox, loader, update,
+    renderer, render_stats, physics_stats, scene, ground_material, ground, light, camera, controls;
+
+initScene = function() {
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMapSoft = true;
+    document.getElementById( 'viewport' ).appendChild( renderer.domElement );
+
+    // CONTROLS
+    controls = new THREE.OrbitControls( camera, renderer.domElement );
    
-   // RENDERER
-   if ( Detector.webgl )
-      renderer = new THREE.WebGLRenderer( {antialias:true} );
-   else
-      renderer = new THREE.CanvasRenderer(); 
-   renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-   container = document.getElementById( 'ThreeJS' );
-   container.appendChild( renderer.domElement );
+
+    render_stats = new Stats();
+    render_stats.domElement.style.position = 'absolute';
+    render_stats.domElement.style.top = '0px';
+    render_stats.domElement.style.zIndex = 100;
+    document.getElementById( 'viewport' ).appendChild( render_stats.domElement );
+
+    physics_stats = new Stats();
+    physics_stats.domElement.style.position = 'absolute';
+    physics_stats.domElement.style.top = '50px';
+    physics_stats.domElement.style.zIndex = 100;
+    document.getElementById( 'viewport' ).appendChild( physics_stats.domElement );
+    scene = new Physijs.Scene;
+    scene.setGravity(new THREE.Vector3( 0, -30, 0 ));
+    scene.addEventListener(
+        'update',
+        function() {
+            scene.simulate( undefined, 1 );
+            physics_stats.update();
+        }
+    );
    
-   // EVENTS
-   THREEx.WindowResize(renderer, camera);
-   THREEx.FullScreen.bindKey({ charCode : 'm'.charCodeAt(0) });
-   
-   // CONTROLS
+    camera = new THREE.PerspectiveCamera(
+        35,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000
+    );
+    camera.position.set( 60, 50, 60 );
+    camera.lookAt( scene.position );
+    scene.add( camera );
+
+    // CONTROLS
    controls = new THREE.OrbitControls( camera, renderer.domElement );
-   
-   // STATS
-   stats = new Stats();
-   stats.domElement.style.position = 'absolute';
-   stats.domElement.style.bottom = '0px';
-   stats.domElement.style.zIndex = 100;
-   container.appendChild( stats.domElement );
-   
-   // LIGHT
-   var light = new THREE.PointLight(0xffffff);
-   light.position.set(0,150,100);
-   scene.add(light);
-   
-   // FLOOR
-   var floorTexture = new THREE.ImageUtils.loadTexture( 'images/checkerboard.jpg' );
-   floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping; 
-   floorTexture.repeat.set( 2, 2 );
-   var floorMaterial = new THREE.MeshBasicMaterial( { map: floorTexture, side: THREE.DoubleSide } );
-   var floorGeometry = new THREE.PlaneGeometry(200, 200, 2, 2);
-   MovingFloor = new THREE.Mesh(floorGeometry, floorMaterial);
-   MovingFloor.position.y = -0.5;
-   MovingFloor.rotation.x = Math.PI / 2;
-   scene.add(MovingFloor);
 
-   // AXES
-   Axes = new THREE.AxisHelper(150);
-   scene.add( Axes )
+    // Light
+    light = new THREE.DirectionalLight( 0xFFFFFF );
+    light.position.set( 20, 40, -15 );
+    light.target.position.copy( scene.position );
+    light.castShadow = true;
+    light.shadowCameraLeft = -60;
+    light.shadowCameraTop = -60;
+    light.shadowCameraRight = 60;
+    light.shadowCameraBottom = 60;
+    light.shadowCameraNear = 20;
+    light.shadowCameraFar = 200;
+    light.shadowBias = -.0001
+    light.shadowMapWidth = light.shadowMapHeight = 2048;
+    light.shadowDarkness = .7;
+    scene.add( light );
+    // Loader
+    loader = new THREE.TextureLoader();
    
-   // SKYBOX
-   var imagePrefix = "images/dawnmountain-";
-   var directions  = ["xpos", "xneg", "ypos", "yneg", "zpos", "zneg"];
-   var imageSuffix = ".png";
-   var skyGeometry = new THREE.CubeGeometry( 5000, 5000, 5000 );  
-   var materialArray = [];
-   for (var i = 0; i < 6; i++)
-      materialArray.push( new THREE.MeshBasicMaterial({
-         map: THREE.ImageUtils.loadTexture( imagePrefix + directions[i] + imageSuffix ),
-         side: THREE.BackSide
-      }));
-   var skyMaterial = new THREE.MeshFaceMaterial( materialArray );
-   var skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
-   scene.add( skyBox );
+    // Ground
+    ground_material = Physijs.createMaterial(
+        new THREE.MeshLambertMaterial({ map: loader.load( 'images/checkerboard.jpg' ) }),
+        .8, // high friction
+        .3 // low restitution
+    );
+    ground_material.map.wrapS = ground_material.map.wrapT = THREE.RepeatWrapping;
+    ground_material.map.repeat.set( 3, 3 );
    
-   ////////////
-   // CUSTOM //
-   ////////////
-   // Spheres
-   //   Note: a standard flat rectangular image will look distorted,
-   //   a "spherical projection" image will look "normal".
-   
-   // radius, segmentsWidth, segmentsHeight
-   var sphereGeom =  new THREE.SphereGeometry( 40, 32, 16 ); 
-   
-   var light2 = new THREE.AmbientLight(0x444444);
-   scene.add(light2);
-   
-   // shaded earth -- side away from light picks up AmbientLight's color.
-   var earthTexture = THREE.ImageUtils.loadTexture( 'images/earth-day.jpg' );
-   var earthMaterial = new THREE.MeshLambertMaterial( { map: earthTexture } );
-   var earth = new THREE.Mesh( sphereGeom.clone(), earthMaterial );
-   earth.position.set(300, 150, 0);
-   scene.add( earth );      
-   
-   // create a small sphere to show position of light
-   var lightbulb = new THREE.Mesh( 
-      new THREE.SphereGeometry( 10, 16, 8 ), 
-      new THREE.MeshBasicMaterial( { color: 0xffaa00 } )
-   );
-   scene.add( lightbulb );
-   lightbulb.position = light.position;
-   
-   // Cubes
-   //   Note: when using a single image, it will appear on each of the faces.
-   //   Six different images (one per face) may be used if desired.
-   
-   // create an array with six textures for a cool cube
-   var materialArray = [];
-   materialArray.push(new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( 'images/dice1.png' ) }));
-   materialArray.push(new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( 'images/dice6.png' ) }));
-   materialArray.push(new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( 'images/dice2.png' ) }));
-   materialArray.push(new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( 'images/dice5.png' ) }));
-   materialArray.push(new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( 'images/dice3.png' ) }));
-   materialArray.push(new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( 'images/dice4.png' ) }));
-   var DiceBlueMaterial = new THREE.MeshFaceMaterial(materialArray);
-   var DiceBlueGeom = new THREE.CubeGeometry( 85, 85, 85, 1, 1, 1 );
-   var DiceBlue = new THREE.Mesh( DiceBlueGeom, DiceBlueMaterial );
-   DiceBlue.position.set(60, 50, -100);
-   scene.add( DiceBlue );   
-}
+    ground = new Physijs.BoxMesh(
+        new THREE.BoxGeometry(100, 1, 100),
+        ground_material,
+        0 // mass
+    );
+    ground.receiveShadow = true;
+    scene.add( ground );
 
-function animate() {
-   requestAnimationFrame( animate );
-   render();      
-   update();
-}
+    spawnBox();
 
-function update() {
-   var delta = clock.getDelta(); // seconds.
-   var moveDistance = 200 * delta; // 200 pixels per second
-   var rotateAngle = Math.PI / 2 * delta;   // pi/2 radians (90 degrees) per second
-   
-   // local coordinates
-   // local transformations
+    requestAnimationFrame( render );
+    scene.simulate();
+};
 
-   if (keyboard.pressed("W")) {
-      MovingFloor.position.y += moveDistance;
-      Axes.position.y += moveDistance;
-   }
-   if (keyboard.pressed("S")) {
-      MovingFloor.position.y -= moveDistance;
-      Axes.position.y -= moveDistance;
-   }
-      
-   // global coordinates
-   if (keyboard.pressed("left")) {
-      MovingFloor.position.x -= moveDistance;
-      Axes.position.x -= moveDistance;
-   }
-   if (keyboard.pressed("right")) {
-      MovingFloor.position.x += moveDistance;
-      Axes.position.x += moveDistance;
-   }
-   if (keyboard.pressed("up")) {
-      MovingFloor.position.z -= moveDistance;
-      Axes.position.z -= moveDistance;
-   }
-   if (keyboard.pressed("down")) {
-      MovingFloor.position.z += moveDistance;
-      Axes.position.z += moveDistance;
-   }
-   
-   controls.update();
-   stats.update();
-}
+spawnBox = (function() {
+    var box_geometry = new THREE.BoxGeometry( 4, 4, 4 ),
+        handleCollision = function( collided_with, linearVelocity, angularVelocity ) {
+            switch ( ++this.collisions ) {
+            
+                case 1:
+                    this.material.color.setHex(0xcc8855);
+                    break;
+            
+                case 2:
+                    this.material.color.setHex(0xbb9955);
+                    break;
+            
+                case 3:
+                    this.material.color.setHex(0xaaaa55);
+                    break;
+            
+                case 4:
+                    this.material.color.setHex(0x99bb55);
+                    break;
+            
+                case 5:
+                    this.material.color.setHex(0x88cc55);
+                    break;
+                
+                case 6:
+                    this.material.color.setHex(0x77dd55);
+                    break;
+            }
+        },
+        createBox = function() {
+            var box, material;
+         
+            material = Physijs.createMaterial(
+                new THREE.MeshLambertMaterial({ map: loader.load( 'images/dice1.png' ) }),
+                .6, // medium friction
+                .3 // low restitution
+            );
+            material.map.wrapS = material.map.wrapT = THREE.RepeatWrapping;
+            material.map.repeat.set( .5, .5 );
 
-function render() {
-   renderer.render( scene, camera );
-}
+            box = new Physijs.BoxMesh(
+                box_geometry,
+                material
+            );
+            box.collisions = 0;
+
+            box.position.set(
+                Math.random() * 15 - 7.5,
+                25,
+                Math.random() * 15 - 7.5
+            );
+
+            box.rotation.set(
+                Math.random() * Math.PI,
+                Math.random() * Math.PI,
+                Math.random() * Math.PI
+            );
+
+            box.castShadow = true;
+            box.addEventListener( 'collision', handleCollision );
+            box.addEventListener( 'ready', spawnBox );
+            scene.add( box );
+        };
+
+    return function() {
+        setTimeout( createBox, 1000 );
+    };
+})();
+
+render = function() {
+    requestAnimationFrame( render );
+    renderer.render( scene, camera );
+    render_stats.update();
+    controls.update();
+};
+
+window.onload = initScene;
