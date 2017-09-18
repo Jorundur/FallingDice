d1b4a8014fce38e9a4647dfa2bbc364ae677bc48
Jorundur <jorundurj@gmail.com>
2017-09-18 12:34:18 +0100

    Rotating cube with one dice texture

--- main.js
+++ main.js
@@ -1,188 +1,461 @@
-/*============= Creating a canvas =================*/
-var canvas = document.getElementById('my_Canvas');
-gl = canvas.getContext('experimental-webgl');
+var cubeRotation = 0.0;
 
-/*============ Defining and storing the geometry =========*/
+main();
 
-var vertices = [
-   -1,-1,-1, 1,-1,-1, 1, 1,-1, -1, 1,-1,
-   -1,-1, 1, 1,-1, 1, 1, 1, 1, -1, 1, 1,
-   -1,-1,-1, -1, 1,-1, -1, 1, 1, -1,-1, 1,
-   1,-1,-1, 1, 1,-1, 1, 1, 1, 1,-1, 1,
-   -1,-1,-1, -1,-1, 1, 1,-1, 1, 1,-1,-1,
-   -1, 1,-1, -1, 1, 1, 1, 1, 1, 1, 1,-1, 
-];
+//
+// Start here
+//
+function main() {
+  const canvas = document.querySelector('#glcanvas');
+  const gl = canvas.getContext('webgl');
 
-var colors = [
-   5,3,7, 5,3,7, 5,3,7, 5,3,7,
-   1,1,3, 1,1,3, 1,1,3, 1,1,3,
-   0,0,1, 0,0,1, 0,0,1, 0,0,1,
-   1,0,0, 1,0,0, 1,0,0, 1,0,0,
-   1,1,0, 1,1,0, 1,1,0, 1,1,0,
-   0,1,0, 0,1,0, 0,1,0, 0,1,0
-];
+  // If we don't have a GL context, give up now
 
-var indices = [
-   0,1,2, 0,2,3, 4,5,6, 4,6,7,
-   8,9,10, 8,10,11, 12,13,14, 12,14,15,
-   16,17,18, 16,18,19, 20,21,22, 20,22,23 
-];
+  if (!gl) {
+    alert('Unable to initialize WebGL. Your browser or machine may not support it.');
+    return;
+  }
 
-// Create and store data into vertex buffer
-var vertex_buffer = gl.createBuffer ();
-gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  // Vertex shader program
 
-// Create and store data into color buffer
-var color_buffer = gl.createBuffer ();
-gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+  const vsSource = `
+    attribute vec4 aVertexPosition;
+    attribute vec2 aTextureCoord;
 
-// Create and store data into index buffer
-var index_buffer = gl.createBuffer ();
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
-gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
-                                     
-/*=================== Shaders =========================*/
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
 
-var vertCode = 'attribute vec3 position;'+
-   'uniform mat4 Pmatrix;'+
-   'uniform mat4 Vmatrix;'+
-   'uniform mat4 Mmatrix;'+
-   'attribute vec3 color;'+//the color of the point
-   'varying vec3 vColor;'+
+    varying highp vec2 vTextureCoord;
 
-   'void main(void) { '+//pre-built function
-      'gl_Position = Pmatrix*Vmatrix*Mmatrix*vec4(position, 1.);'+
-      'vColor = color;'+
-   '}';
+    void main(void) {
+      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+      vTextureCoord = aTextureCoord;
+    }
+  `;
 
-var fragCode = 'precision mediump float;'+
-   'varying vec3 vColor;'+
-   'void main(void) {'+
-      'gl_FragColor = vec4(vColor, 1.);'+
-   '}';
+  // Fragment shader program
 
-var vertShader = gl.createShader(gl.VERTEX_SHADER);
-gl.shaderSource(vertShader, vertCode);
-gl.compileShader(vertShader);
+  const fsSource = `
+    varying highp vec2 vTextureCoord;
 
-var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-gl.shaderSource(fragShader, fragCode);
-gl.compileShader(fragShader);
+    uniform sampler2D uSampler;
 
-var shaderProgram = gl.createProgram();
-gl.attachShader(shaderProgram, vertShader);
-gl.attachShader(shaderProgram, fragShader);
-gl.linkProgram(shaderProgram);
+    void main(void) {
+      gl_FragColor = texture2D(uSampler, vTextureCoord);
+    }
+  `;
 
-/* ====== Associating attributes to vertex shader =====*/
-var Pmatrix = gl.getUniformLocation(shaderProgram, "Pmatrix");
-var Vmatrix = gl.getUniformLocation(shaderProgram, "Vmatrix");
-var Mmatrix = gl.getUniformLocation(shaderProgram, "Mmatrix");
+  // Initialize a shader program; this is where all the lighting
+  // for the vertices and so forth is established.
+  const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
 
-gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-var position = gl.getAttribLocation(shaderProgram, "position");
-gl.vertexAttribPointer(position, 3, gl.FLOAT, false,0,0) ;
+  // Collect all the info needed to use the shader program.
+  // Look up which attributes our shader program is using
+  // for aVertexPosition, aTextureCoord and also
+  // look up uniform locations.
+  const programInfo = {
+    program: shaderProgram,
+    attribLocations: {
+      vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+      textureCoord: gl.getAttribLocation(shaderProgram, 'aTextureCoord'),
+    },
+    uniformLocations: {
+      projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+      modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+      uSampler: gl.getUniformLocation(shaderProgram, 'uSampler'),
+    },
+  };
 
-// Position
-gl.enableVertexAttribArray(position);
-gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
-var color = gl.getAttribLocation(shaderProgram, "color");
-gl.vertexAttribPointer(color, 3, gl.FLOAT, false,0,0) ;
+  // Here's where we call the routine that builds all the
+  // objects we'll be drawing.
+  const buffers = initBuffers(gl);
 
-// Color
-gl.enableVertexAttribArray(color);
-gl.useProgram(shaderProgram);
+  const texture = loadTexture(gl, 'dice1.png');
 
-/*==================== MATRIX =====================*/
+  var then = 0;
 
-function get_projection(angle, a, zMin, zMax) {
-   var ang = Math.tan((angle*.5)*Math.PI/180);//angle*.5
-   return [
-      0.5/ang, 0 , 0, 0,
-      0, 0.5*a/ang, 0, 0,
-      0, 0, -(zMax+zMin)/(zMax-zMin), -1,
-      0, 0, (-2*zMax*zMin)/(zMax-zMin), 0 
-   ];
+  // Draw the scene repeatedly
+  function render(now) {
+    now *= 0.001;  // convert to seconds
+    const deltaTime = now - then;
+    then = now;
+
+    drawScene(gl, programInfo, buffers, texture, deltaTime);
+
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
 }
 
-var proj_matrix = get_projection(40, canvas.width/canvas.height, 1, 100);
+//
+// initBuffers
+//
+// Initialize the buffers we'll need. For this demo, we just
+// have one object -- a simple three-dimensional cube.
+//
+function initBuffers(gl) {
 
-var mov_matrix = [1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1];
-var view_matrix = [1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1];
+  // Create a buffer for the cube's vertex positions.
 
-// translating z
-view_matrix[14] = view_matrix[14]-6;//zoom
+  const positionBuffer = gl.createBuffer();
 
-/*==================== Rotation ====================*/
+  // Select the positionBuffer as the one to apply buffer
+  // operations to from here out.
 
-function rotateZ(m, angle) {
-   var c = Math.cos(angle);
-   var s = Math.sin(angle);
-   var mv0 = m[0], mv4 = m[4], mv8 = m[8];
-	
-   m[0] = c*m[0]-s*m[1];
-   m[4] = c*m[4]-s*m[5];
-   m[8] = c*m[8]-s*m[9];
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-   m[1]=c*m[1]+s*mv0;
-   m[5]=c*m[5]+s*mv4;
-   m[9]=c*m[9]+s*mv8;
+  // Now create an array of positions for the cube.
+
+  const positions = [
+    // Front face
+    -1.0, -1.0,  1.0,
+     1.0, -1.0,  1.0,
+     1.0,  1.0,  1.0,
+    -1.0,  1.0,  1.0,
+
+    // Back face
+    -1.0, -1.0, -1.0,
+    -1.0,  1.0, -1.0,
+     1.0,  1.0, -1.0,
+     1.0, -1.0, -1.0,
+
+    // Top face
+    -1.0,  1.0, -1.0,
+    -1.0,  1.0,  1.0,
+     1.0,  1.0,  1.0,
+     1.0,  1.0, -1.0,
+
+    // Bottom face
+    -1.0, -1.0, -1.0,
+     1.0, -1.0, -1.0,
+     1.0, -1.0,  1.0,
+    -1.0, -1.0,  1.0,
+
+    // Right face
+     1.0, -1.0, -1.0,
+     1.0,  1.0, -1.0,
+     1.0,  1.0,  1.0,
+     1.0, -1.0,  1.0,
+
+    // Left face
+    -1.0, -1.0, -1.0,
+    -1.0, -1.0,  1.0,
+    -1.0,  1.0,  1.0,
+    -1.0,  1.0, -1.0,
+  ];
+
+  // Now pass the list of positions into WebGL to build the
+  // shape. We do this by creating a Float32Array from the
+  // JavaScript array, then use it to fill the current buffer.
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+  // Now set up the texture coordinates for the faces.
+
+  const textureCoordBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+
+  const textureCoordinates = [
+    // Front
+    0.0,  0.0,
+    1.0,  0.0,
+    1.0,  1.0,
+    0.0,  1.0,
+    // Back
+    0.0,  0.0,
+    1.0,  0.0,
+    1.0,  1.0,
+    0.0,  1.0,
+    // Top
+    0.0,  0.0,
+    1.0,  0.0,
+    1.0,  1.0,
+    0.0,  1.0,
+    // Bottom
+    0.0,  0.0,
+    1.0,  0.0,
+    1.0,  1.0,
+    0.0,  1.0,
+    // Right
+    0.0,  0.0,
+    1.0,  0.0,
+    1.0,  1.0,
+    0.0,  1.0,
+    // Left
+    0.0,  0.0,
+    1.0,  0.0,
+    1.0,  1.0,
+    0.0,  1.0,
+  ];
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates),
+                gl.STATIC_DRAW);
+
+  // Build the element array buffer; this specifies the indices
+  // into the vertex arrays for each face's vertices.
+
+  const indexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+  // This array defines each face as two triangles, using the
+  // indices into the vertex array to specify each triangle's
+  // position.
+
+  const indices = [
+    0,  1,  2,      0,  2,  3,    // front
+    4,  5,  6,      4,  6,  7,    // back
+    8,  9,  10,     8,  10, 11,   // top
+    12, 13, 14,     12, 14, 15,   // bottom
+    16, 17, 18,     16, 18, 19,   // right
+    20, 21, 22,     20, 22, 23,   // left
+  ];
+
+  // Now send the element array to GL
+
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
+      new Uint16Array(indices), gl.STATIC_DRAW);
+
+  return {
+    position: positionBuffer,
+    textureCoord: textureCoordBuffer,
+    indices: indexBuffer,
+  };
 }
 
-function rotateX(m, angle) {
-   var c = Math.cos(angle);
-   var s = Math.sin(angle);
-   var mv1 = m[1], mv5 = m[5], mv9 = m[9];
-	
-   m[1] = m[1]*c-m[2]*s;
-   m[5] = m[5]*c-m[6]*s;
-   m[9] = m[9]*c-m[10]*s;
+//
+// Initialize a texture and load an image.
+// When the image finished loading copy it into the texture.
+//
+function loadTexture(gl, url) {
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
 
-   m[2] = m[2]*c+mv1*s;
-   m[6] = m[6]*c+mv5*s;
-   m[10] = m[10]*c+mv9*s;
+  // Because images have to be download over the internet
+  // they might take a moment until they are ready.
+  // Until then put a single pixel in the texture so we can
+  // use it immediately. When the image has finished downloading
+  // we'll update the texture with the contents of the image.
+  const level = 0;
+  const internalFormat = gl.RGBA;
+  const width = 1;
+  const height = 1;
+  const border = 0;
+  const srcFormat = gl.RGBA;
+  const srcType = gl.UNSIGNED_BYTE;
+  const pixel = new Uint8Array([0, 0, 255, 255]);  // opaque blue
+  gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                width, height, border, srcFormat, srcType,
+                pixel);
+
+  const image = new Image();
+  image.onload = function() {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                  srcFormat, srcType, image);
+
+    // WebGL1 has different requirements for power of 2 images
+    // vs non power of 2 images so check if the image is a
+    // power of 2 in both dimensions.
+    if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+       // Yes, it's a power of 2. Generate mips.
+       gl.generateMipmap(gl.TEXTURE_2D);
+    } else {
+       // No, it's not a power of 2. Turn of mips and set
+       // wrapping to clamp to edge
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    }
+  };
+  image.src = url;
+
+  return texture;
 }
 
-function rotateY(m, angle) {
-   var c = Math.cos(angle);
-   var s = Math.sin(angle);
-   var mv0 = m[0], mv4 = m[4], mv8 = m[8];
-	
-   m[0] = c*m[0]+s*m[2];
-   m[4] = c*m[4]+s*m[6];
-   m[8] = c*m[8]+s*m[10];
-
-   m[2] = c*m[2]-s*mv0;
-   m[6] = c*m[6]-s*mv4;
-   m[10] = c*m[10]-s*mv8;
+function isPowerOf2(value) {
+  return (value & (value - 1)) == 0;
 }
 
-/*================= Drawing ===========================*/
-var time_old = 0;
+//
+// Draw the scene.
+//
+function drawScene(gl, programInfo, buffers, texture, deltaTime) {
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
+  gl.clearDepth(1.0);                 // Clear everything
+  gl.enable(gl.DEPTH_TEST);           // Enable depth testing
+  gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
 
-var animate = function(time) {
+  // Clear the canvas before we start drawing on it.
 
-   var dt = time-time_old;
-   rotateZ(mov_matrix, dt*0.005);//time
-   rotateY(mov_matrix, dt*0.002);
-   rotateX(mov_matrix, dt*0.003);
-   time_old = time;
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-   gl.enable(gl.DEPTH_TEST);
-   gl.depthFunc(gl.LEQUAL);
-   gl.clearColor(0.5, 0.5, 0.5, 0.9);
-   gl.clearDepth(1.0);
+  // Create a perspective matrix, a special matrix that is
+  // used to simulate the distortion of perspective in a camera.
+  // Our field of view is 45 degrees, with a width/height
+  // ratio that matches the display size of the canvas
+  // and we only want to see objects between 0.1 units
+  // and 100 units away from the camera.
 
-   gl.viewport(0.0, 0.0, canvas.width, canvas.height);
-   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-   gl.uniformMatrix4fv(Pmatrix, false, proj_matrix);
-   gl.uniformMatrix4fv(Vmatrix, false, view_matrix);
-   gl.uniformMatrix4fv(Mmatrix, false, mov_matrix);
-   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
-   gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
-	
-   window.requestAnimationFrame(animate);
+  const fieldOfView = 45 * Math.PI / 180;   // in radians
+  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+  const zNear = 0.1;
+  const zFar = 100.0;
+  const projectionMatrix = mat4.create();
+
+  // note: glmatrix.js always has the first argument
+  // as the destination to receive the result.
+  mat4.perspective(projectionMatrix,
+                   fieldOfView,
+                   aspect,
+                   zNear,
+                   zFar);
+
+  // Set the drawing position to the "identity" point, which is
+  // the center of the scene.
+  const modelViewMatrix = mat4.create();
+
+  // Now move the drawing position a bit to where we want to
+  // start drawing the square.
+
+  mat4.translate(modelViewMatrix,     // destination matrix
+                 modelViewMatrix,     // matrix to translate
+                 [-0.0, 0.0, -6.0]);  // amount to translate
+  mat4.rotate(modelViewMatrix,  // destination matrix
+              modelViewMatrix,  // matrix to rotate
+              cubeRotation,     // amount to rotate in radians
+              [0, 0, 1]);       // axis to rotate around (Z)
+  mat4.rotate(modelViewMatrix,  // destination matrix
+              modelViewMatrix,  // matrix to rotate
+              cubeRotation * .7,// amount to rotate in radians
+              [0, 1, 0]);       // axis to rotate around (X)
+
+  // Tell WebGL how to pull out the positions from the position
+  // buffer into the vertexPosition attribute
+  {
+    const numComponents = 3;
+    const type = gl.FLOAT;
+    const normalize = false;
+    const stride = 0;
+    const offset = 0;
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+    gl.vertexAttribPointer(
+        programInfo.attribLocations.vertexPosition,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset);
+    gl.enableVertexAttribArray(
+        programInfo.attribLocations.vertexPosition);
+  }
+
+  // Tell WebGL how to pull out the texture coordinates from
+  // the texture coordinate buffer into the textureCoord attribute.
+  {
+    const numComponents = 2;
+    const type = gl.FLOAT;
+    const normalize = false;
+    const stride = 0;
+    const offset = 0;
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.textureCoord);
+    gl.vertexAttribPointer(
+        programInfo.attribLocations.textureCoord,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset);
+    gl.enableVertexAttribArray(
+        programInfo.attribLocations.textureCoord);
+  }
+
+  // Tell WebGL which indices to use to index the vertices
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
+
+  // Tell WebGL to use our program when drawing
+
+  gl.useProgram(programInfo.program);
+
+  // Set the shader uniforms
+
+  gl.uniformMatrix4fv(
+      programInfo.uniformLocations.projectionMatrix,
+      false,
+      projectionMatrix);
+  gl.uniformMatrix4fv(
+      programInfo.uniformLocations.modelViewMatrix,
+      false,
+      modelViewMatrix);
+
+  // Specify the texture to map onto the faces.
+
+  // Tell WebGL we want to affect texture unit 0
+  gl.activeTexture(gl.TEXTURE0);
+
+  // Bind the texture to texture unit 0
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // Tell the shader we bound the texture to texture unit 0
+  gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
+
+  {
+    const vertexCount = 36;
+    const type = gl.UNSIGNED_SHORT;
+    const offset = 0;
+    gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+  }
+
+  // Update the rotation for the next draw
+
+  cubeRotation += deltaTime;
 }
-animate(0);
+
+//
+// Initialize a shader program, so WebGL knows how to draw our data
+//
+function initShaderProgram(gl, vsSource, fsSource) {
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+  // Create the shader program
+
+  const shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  // If creating the shader program failed, alert
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+    return null;
+  }
+
+  return shaderProgram;
+}
+
+//
+// creates a shader of the given type, uploads the source and
+// compiles it.
+//
+function loadShader(gl, type, source) {
+  const shader = gl.createShader(type);
+
+  // Send the source to the shader object
+
+  gl.shaderSource(shader, source);
+
+  // Compile the shader program
+
+  gl.compileShader(shader);
+
+  // See if it compiled successfully
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
+}
